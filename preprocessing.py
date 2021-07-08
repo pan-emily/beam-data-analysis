c437ecc283ca73818c6ff760391d3886e2d52b37
emilypan <emilypan@caltech.edu>
2021-07-08 14:19:51 -0400

    fixed typo

--- preprocessing.py
+++ preprocessing.py
@@ -317,7 +317,7 @@ def get_scan_files(folder_path, file_name, start, end):
 
 def find_end_scan(folder_path, file_name):
     file_num = 1
-    while os.path.isfile(gen_data_filepath(folder_path, file_name, file_num)):
+    while os.path.isfile(get_data_filepath(folder_path, file_name, file_num)):
         file_num+=1
     return file_num-1
 
